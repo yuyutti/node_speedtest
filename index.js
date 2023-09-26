81d68d0dc7c1b07c84e5c1209ce082704980ae15
yuyutti <yuyutti@yuyutti.com>
2023-09-26 19:17:18 +0900

    commits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.static('public'));
 app.use('/files', express.static('files'));
