966be452ed800f22c38a78fe8e61580ac43bf320
yuyutti <yuyutti@yuyutti.com>
2023-09-26 23:50:11 +0900

    commits

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.static('public'));
-app.use('/files', express.static('files'));
 
-app.get('/download-for-time', (req, res) => {
-    const chunkSize = 1024 * 1024 * 10; // 10MB
-    res.setHeader('Content-Length', chunkSize);
-    res.write(Buffer.alloc(chunkSize, '0'));
-    res.end();
+// Pingテストのルート
+app.get('/ping', (req, res) => {
+    res.status(200).end();
 });
 
+// ダウンロードテストのルート
+app.get('/download', (req, res) => {
+    const randomSize = Math.floor(Math.random() * (100 * 1024 * 1024 - 1024 + 1) + 1024);
+    const randomData = Buffer.alloc(randomSize, () => Math.floor(Math.random() * 256));
+    res.send(randomData);
+});
 
+// アップロードテストのルート
+app.post('/upload', (req, res) => {
+    req.on('data', (chunk) => {
+        // この例ではアップロードされたデータを処理せず破棄します。
+        // 必要に応じてデータの処理を追加することができます。
+    });
+    req.on('end', () => {
+        res.status(200).end();
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
