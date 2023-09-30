acd27b6962b27928431f4ccf921493a865eeafe7
yuyutti <yuyutti@yuyutti.com>
2023-09-30 19:57:42 +0900

    commits

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
+
+app.use(express.json({ limit: '10mb' })); // ボディのサイズを増やす
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(express.static('public'));
 
-const dataPool = [];
-const minSize = 1 * 1024 * 1024; // 1MB
-const maxSize = 50 * 1024 * 1024; // 50MB
-const numDataItems = 50; // データアイテムの数
-
-for (let i = 0; i < numDataItems; i++) {
-    const randomSize = Math.floor(Math.random() * (maxSize - minSize + 1) + minSize);
-    const randomData = Buffer.alloc(randomSize, () => Math.floor(Math.random() * 256));
-    dataPool.push(randomData);
-}
-
-// Pingテストのルート
 app.get('/ping', (req, res) => {
-    res.status(200).end();
+    res.send('pong');
 });
 
-// ダウンロードテストのルート
-app.get('/download', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * dataPool.length);
-    const randomData = dataPool[randomIndex];
-    const dataSizeMB = (randomData.length / (1024 * 1024)).toFixed(2);
-    console.log(`UP: ${dataSizeMB} MB`)
-    res.send(randomData);
+app.get('/download-test', (req, res) => {
+    const dataSize = parseInt(req.query.size, 10) || 1e6; // リクエストに応じたサイズのデータを生成
+    const data = 'A'.repeat(dataSize);
+    res.send(data);
 });
 
-// アップロードテストのルート
-app.post('/upload', (req, res) => {
-    let receivedDataSize = 0;
-    req.on('data', (chunk) => {
-        receivedDataSize += chunk.length;
-    });
-    req.on('end', () => {
-        const dataSizeMB = (receivedDataSize / (1024 * 1024)).toFixed(2);
-        console.log(`Down: ${dataSizeMB} MB`);
-        res.status(200).end();
-    });
+app.post('/upload-test', (req, res) => {
+    res.send('Data received');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server started on http://localhost:${PORT}`);
 });
