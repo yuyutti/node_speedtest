c419a7de0fe7ff16593e5b0aee70344598ce0d45
yuyutti <yuyutti@yuyutti.com>
2023-09-30 19:58:59 +0900

    4000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json({ limit: '10mb' })); // ボディのサイズを増やす
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
